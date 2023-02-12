d5d84659cd837f8bfb6f87196864ddb148cd83ce
Sofia <sofiarodrigues45@gmail.com>
2023-02-11 21:54:41 -0300

    finalizando desafio

---
+++ src/screens/RegisterLoginData/index.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Alert, KeyboardAvoidingView, Platform } from 'react-native';
+import { useForm } from 'react-hook-form';
+import { RFValue } from 'react-native-responsive-fontsize';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+
+import {
+  Container,
+  Form
+} from './styles';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+interface FormData {
+  service_name: string;
+  email: string;
+  password: string;
+}
+
+const schema = Yup.object().shape({
+  service_name: Yup.string().required('Nome do serviço é obrigatório!'),
+  email: Yup.string().email('Não é um email válido').required('Email é obrigatório!'),
+  password: Yup.string().required('Senha é obrigatória!'),
+})
+
+type RootStackParamList = {
+  Home: undefined;
+  RegisterLoginData: undefined;
+};
+
+type NavigationProps = StackNavigationProp<RootStackParamList, 'RegisterLoginData'>;
+
+export function RegisterLoginData() {
+  const { navigate } = useNavigation<NavigationProps>()
+  const {
+    control,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  async function handleRegister(formData: FormData) {
+    const newLoginData = {
+      id: String(uuid.v4()),
+      ...formData
+    }
+
+    const dataKey = '@savepass:logins';
+
+    try {
+      const value = await AsyncStorage.getItem(dataKey);
+
+      if (value === null) {
+        return
+      }
+  
+      const parsedValue = JSON.parse(value) || []
+  
+      const newLoginListData = [
+        ...parsedValue,
+        newLoginData
+      ]
+  
+      await AsyncStorage.setItem(dataKey, JSON.stringify(newLoginListData));
+  
+      navigate('Home')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <Header />
+      <Container>
+        <Form>
+          <Input
+            testID="service-name-input"
+            title="Nome do serviço"
+            name="service_name"
+            error={String(errors.service_name && errors.service_name.message)}
+            control={control}
+            autoCapitalize="sentences"
+            autoCorrect
+          />
+          <Input
+            testID="email-input"
+            title="E-mail ou usuário"
+            name="email"
+            error={String(errors.email && errors.email.message)}
+            control={control}
+            autoCorrect={false}
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <Input
+            testID="password-input"
+            title="Senha"
+            name="password"
+            error={String(errors.password && errors.password.message)}
+            control={control}
+            secureTextEntry
+          />
+
+          <Button
+            style={{
+              marginTop: RFValue(8)
+            }}
+            title="Salvar"
+            onPress={handleSubmit(handleRegister)}
+          />
+        </Form>
+      </Container>
+    </KeyboardAvoidingView>
+  )
+}
